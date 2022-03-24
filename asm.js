4070568f8107b05fe241e43473973183af3c4619
Nikola Bebić <nikola.bebic99@gmail.com>
2022-03-24 20:34:01 +0100

    better assembler

--- asm.js
+++ asm.js
@@ -5,6 +5,10 @@ function asm_load() {
     monaco.languages.register({ id: 'z80asm' });
     monaco.languages.setMonarchTokensProvider('z80asm', MONARCH);
     monaco.languages.registerHoverProvider('z80asm', { provideHover: asm_hover_provider });
+    monaco.languages.registerDocumentFormattingEditProvider('z80asm', {
+      displayName: 'Format using ASM80',
+      provideDocumentFormattingEdits: asm_provide_document_formatting_edits
+    })
 
     ASM_EDITOR = monaco.editor.create(document.getElementById('assembler-input'), {
       theme: 'vs-dark',
@@ -21,6 +25,7 @@ function asm_load() {
 
     setTimeout(() => {
       document.getElementById("assembler-loading").remove();
+      do_compile();
     }, 0);
   });
 }
@@ -44,8 +49,32 @@ function asm_hover_provider(model, position, token) {
     return null;
 
   return {
-    contents: [{ value: man }]
+    contents: [{ value: man, isTrusted: true }],
+    range: {
+      startColumn: word.startColumn,
+      endColumn: word.endColumn,
+      startLineNumber: position.lineNumber,
+      endLineNumber: position.lineNumber,
+    }
   };
+}
+
+function asm_provide_document_formatting_edits(model, options, token) {
+  let result = [];
+  model.getLinesContent().forEach((l, i) => {
+    let beautified = ASM.beautify(l, Monolith.Z80);
+    result.push({
+      range: {
+        startColumn: 1,
+        endColumn: l.length + 1,
+        startLineNumber: i + 1,
+        endLineNumber: i + 1,
+      },
+      text: beautified.replace('\n', ''),
+    })
+  })
+
+  return result;
 }
 
 function asm_setStatus(s) {
@@ -65,9 +94,9 @@ function do_compile() {
       source: 'z80asm',
       code: 'err',
       severity: 8,
-      startLineNumber: res[0].s.numline - 1,
+      startLineNumber: res[0].s.numline,
       startColumn: 0,
-      endLineNumber: res[0].s.numline - 1,
+      endLineNumber: res[0].s.numline,
       endColumn: res[0].s.line.length,
     }]);
     asm_setStatus(`Error on line ${res[0].s.numline}: ${res[0].msg}`);
@@ -89,6 +118,10 @@ function do_assemble(do_run) {
 const MONARCH = {
   ignoreCase: true,
 
+  brackets: [
+    ['(', ')', 'delimiter.parenthesis']
+  ],
+
   mnemonic: [
     'ADC', 'ADD', 'AND', 'BIT', 'CALL', 'CCF', 'CP', 'CPD', 'CPDR', 'CPI',
     'CPIR', 'CPL', 'DAA', 'DEC', 'DI', 'DJNZ', 'EI', 'EX', 'EXX', 'HALT',
@@ -106,18 +139,25 @@ const MONARCH = {
     'CSTR', 'PSTR', 'ISTR', 'INCLUDE',
     'ORG', 'ENT', 'ALIGN', 'PHASE', 'DEPHASE',
     'SET', 'IF', 'IFN', 'ELSE', 'ENDIF', 'MACRO', 'REPT',
-    'ENDM', 'BLOCK', 'ENDBLOCK'
-  ],
+    'ENDM', 'BLOCK', 'ENDBLOCK', 'PRAGMA'
+  ].flatMap(x => [x, '.' + x]),
 
   tokenizer: {
     root: [
-      [/^[A-Z_.][A-Z_.0-9]*:/, 'type'],
+      [/^\s*[A-Z_.][A-Z_.0-9]*:/, 'type'],
 
-      [/\b([ABCDEFHL]|AF'?|HL'?|DE'?|BC'?)\b/, 'constant'],
+      [/\b([ABCDEFHLRMI]|AF'?|HL'?|DE'?|BC'?)\b/, 'constant'],
+      [/\bI[XY][HL]?\b/, 'constant'],
       [/\((HL|DE|BC)\)/, 'constant'],
 
-      [/\b(RET|J[PR]|CALL)\s+([cmpz]|n[cz]|p[eo])\b/, 'keyword.mnemonic'],
+      [/\b(RET|J[PR]|CALL)\s+([CMPZ]|N[CZ]|P[EO])\b/, 'keyword.mnemonic'],
 
+      [/\.[a-z]+/, {
+        cases: {
+          '@preprocessor': 'annotation',
+          '': 'identifier',
+        }
+      }],
       [/\b[a-z][a-z0-9]*\b/, {
         cases: {
           '@mnemonic': 'keyword.mnemonic',
@@ -137,77 +177,158 @@ const MONARCH = {
       [/'[^']*'/, 'string'],
       [/"[^']*"/, 'string'],
 
-      [/[ \t\r\n]/, 'whitespace']
+      [/[ \t\r\n]+/, 'whitespace']
     ]
   }
 };
 
 const MAN_PAGE = {
-  'ADC': 'ADD WITH CARRY',
-  'ADD': 'ADD',
-  'AND': 'LOGICAL AND',
-  'BIT': 'BIT TEST',
-  'CALL': 'CALL SUB ROUTINE',
-  'CCF': 'COMPLEMENT CARRY FLAG',
-  'CP': 'COMPARE',
-  'CPD': 'COMPARE AND DECREMENT',
-  'CPDR': 'COMPARE DECREMENT AND REPEAT',
-  'CPI': 'COMPARE AND INCREMENT',
-  'CPIR': 'COMPARE INCREMENT AND REPEAT',
-  'CPL': 'COMPLEMENT ACCUMULATOR',
-  'DAA': 'DECIMAL ADJUST ACCUMULATOR',
-  'DEC': 'DECREMENT',
-  'DI': 'DISABLE INTERRUPTS',
-  'DJNZ': 'DEC JUMP NON-ZERO',
-  'EI': 'ENABLE INTERRUPTS',
-  'EX': 'EXCHANGE REGISTER PAIR',
-  'EXX': 'EXCHANGE ALTERNATE REGISTERS',
-  'HALT': 'HALT, WAIT FOR INTERRUPT OR RESET',
-  'IM': 'INTERRUPT MODE 0 1 2',
-  'IN': 'INPUT FROM PORT',
-  'INC': 'INCREMENT',
-  'IND': 'INPUT, DEC HL, DEC B',
-  'INDR': 'INPUT, DEC HL, DEC B, REPEAT IF B>0',
-  'INI': 'INPUT, INC HL, DEC B',
-  'INIR': 'INPUT, INC HL, DEC B, REPEAT IF B>0',
-  'JP': 'JUMP',
-  'JR': 'JUMP RELATIVE',
-  'LD': 'LOAD DATA TO/FROM REGISTERS/MEMORY',
-  'LDD': 'LOAD DECREMENT',
-  'LDDR': 'LOAD DECREMENT AND REPEAT',
-  'LDI': 'LOAD AND INCREMENT',
-  'LDIR': 'LOAD INCREMENT AND REPEAT',
-  'NEG': 'NEGATE ACCUMULATOR \'S COMPLEMENT',
-  'NOP': 'NO OPERATION',
-  'OR': 'LOGICAL OR',
-  'OTDR': 'OUTPUT, DEC HL, DEC B, REPEAT IF B>0',
-  'OTIR': 'OUTPUT, INC HL, DEC B, REPEAT IF B>0',
-  'OUT': 'OUTPUT TO PORT',
-  'OUTD': 'OUTPUT, DEC HL, DEC B',
-  'OUTI': 'OUTPUT, INC HL, DEC B',
-  'POP': 'POP FROM STACK',
-  'PUSH': 'PUSH INTO STACK',
-  'RES': 'RESET BIT',
-  'RET': 'RETURN FROM SUB ROUTINE',
-  'RETI': 'RETURN FROM INTERRUPT',
-  'RETN': 'RETURN FROM NON MASKABEL INTERRUPT',
-  'RL': 'ROTATE LEFT register',
-  'RLA': 'ROTATE LEFT ACUMULATOR',
-  'RLC': 'ROTATE LEFT THROUGH CARRY register',
-  'RLCA': 'ROTATE LEFT THROUGH CARRY ACCUMULATUR',
-  'RLD': 'ROTATE LEFT DIGIT',
-  'RR': 'ROTATE RIGHT register',
-  'RRA': 'ROTATE RIGHT ACCUMULATOR',
-  'RRC': 'ROTATE RIGHT CIRCULAR register',
-  'RRCA': 'ROTATE RIGHT CIRCULAR ACCUMULATOR',
-  'RRD': 'ROTATE RIGHT DIGIT',
-  'RST': 'RESTART',
-  'SBC': 'SUBTRACT WITH CARRY',
-  'SCF': 'SET CARRY FLAG',
-  'SET': 'SET BIT',
-  'SLA': 'SHIFT LEFT ARITHMETIC register',
-  'SRA': 'SHIFT RIGHT ARITHMETIC register',
-  'SRL': 'SHIFT RIGHT LOGICAL register',
-  'SUB': 'SUBTRACTION',
-  'XOR': 'EXCLUSIVE OR ',
+  'ADC': `## ADD WITH CARRY
+`,
+  'ADD': `## ADD
+`,
+  'AND': `## LOGICAL AND
+`,
+  'BIT': `## BIT TEST
+`,
+  'CALL': `## CALL SUB ROUTINE
+`,
+  'CCF': `## COMPLEMENT CARRY FLAG
+* **Operation**: CY = Inv CY
+* **Instruction Format**:
+  * Opcode: \`CCF\`
+  * OBJ: \`3Fh\`
+* **Description**:
+  * The Carry flag in the flags register is inverted.
+* **Tstates**: 4.
+* **Flags**:
+  * S: Not affected.
+  * Z: Not affected.
+  * H: Pprevious carry will be copied to H.
+  * P/V: Not affected.
+  * N: Reset.
+  * C: Set if carry was 0 before operation, reset otherwise.
+`,
+  'CP': `## COMPARE
+`,
+  'CPD': `## COMPARE AND DECREMENT
+`,
+  'CPDR': `## COMPARE DECREMENT AND REPEAT
+`,
+  'CPI': `## COMPARE AND INCREMENT
+`,
+  'CPIR': `## COMPARE INCREMENT AND REPEAT
+`,
+  'CPL': `## COMPLEMENT ACCUMULATOR
+`,
+  'DAA': `## DECIMAL ADJUST ACCUMULATOR
+`,
+  'DEC': `## DECREMENT
+`,
+  'DI': `## DISABLE INTERRUPTS
+`,
+  'DJNZ': `## DEC JUMP NON-ZERO
+`,
+  'EI': `## ENABLE INTERRUPTS
+`,
+  'EX': `## EXCHANGE REGISTER PAIR
+`,
+  'EXX': `## EXCHANGE ALTERNATE REGISTERS
+`,
+  'HALT': `## HALT, WAIT FOR INTERRUPT OR RESET
+`,
+  'IM': `## INTERRUPT MODE 0 1 2
+`,
+  'IN': `## INPUT FROM PORT
+`,
+  'INC': `## INCREMENT
+`,
+  'IND': `## INPUT, DEC HL, DEC B
+`,
+  'INDR': `## INPUT, DEC HL, DEC B, REPEAT IF B>0
+`,
+  'INI': `## INPUT, INC HL, DEC B
+`,
+  'INIR': `## INPUT, INC HL, DEC B, REPEAT IF B>0
+`,
+  'JP': `## JUMP
+`,
+  'JR': `## JUMP RELATIVE
+`,
+  'LD': `## LOAD DATA TO/FROM REGISTERS/MEMORY
+`,
+  'LDD': `## LOAD DECREMENT
+`,
+  'LDDR': `## LOAD DECREMENT AND REPEAT
+`,
+  'LDI': `## LOAD AND INCREMENT
+`,
+  'LDIR': `## LOAD INCREMENT AND REPEAT
+`,
+  'NEG': `## NEGATE ACCUMULATOR \'S COMPLEMENT
+`,
+  'NOP': `## NO OPERATION
+`,
+  'OR': `## LOGICAL OR
+`,
+  'OTDR': `## OUTPUT, DEC HL, DEC B, REPEAT IF B>0
+`,
+  'OTIR': `## OUTPUT, INC HL, DEC B, REPEAT IF B>0
+`,
+  'OUT': `## OUTPUT TO PORT
+`,
+  'OUTD': `## OUTPUT, DEC HL, DEC B
+`,
+  'OUTI': `## OUTPUT, INC HL, DEC B
+`,
+  'POP': `## POP FROM STACK
+`,
+  'PUSH': `## PUSH INTO STACK
+`,
+  'RES': `## RESET BIT
+`,
+  'RET': `## RETURN FROM SUB ROUTINE
+`,
+  'RETI': `## RETURN FROM INTERRUPT
+`,
+  'RETN': `## RETURN FROM NON MASKABEL INTERRUPT
+`,
+  'RL': `## ROTATE LEFT register
+`,
+  'RLA': `## ROTATE LEFT ACUMULATOR
+`,
+  'RLC': `## ROTATE LEFT THROUGH CARRY register
+`,
+  'RLCA': `## ROTATE LEFT THROUGH CARRY ACCUMULATUR
+`,
+  'RLD': `## ROTATE LEFT DIGIT
+`,
+  'RR': `## ROTATE RIGHT register
+`,
+  'RRA': `## ROTATE RIGHT ACCUMULATOR
+`,
+  'RRC': `## ROTATE RIGHT CIRCULAR register
+`,
+  'RRCA': `## ROTATE RIGHT CIRCULAR ACCUMULATOR
+`,
+  'RRD': `## ROTATE RIGHT DIGIT
+`,
+  'RST': `## RESTART
+`,
+  'SBC': `## SUBTRACT WITH CARRY
+`,
+  'SCF': `## SET CARRY FLAG
+`,
+  'SET': `## SET BIT
+`,
+  'SLA': `## SHIFT LEFT ARITHMETIC register
+`,
+  'SRA': `## SHIFT RIGHT ARITHMETIC register
+`,
+  'SRL': `## SHIFT RIGHT LOGICAL register
+`,
+  'SUB': `## SUBTRACTION
+`,
+  'XOR': `## EXCLUSIVE OR 
+`,
 }
